a3914c4a6b295364803b92f36e14cc782b09dfa9
Mehmet ALPEREN <me.alperen@gmail.com>
2022-06-18 20:11:49 +0300

    Project initialized and context is added.

---
+++ src/context.jsx
@@ -0,0 +1,13 @@
+import React, {useContext, useState} from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
